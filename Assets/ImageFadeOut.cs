51de983aa6a11e11d31c058e528f0874f77fc537
Matěj Hes <hesmatej1@gmail.com>
2025-05-28 23:58:58 +0200

    child walking

--- Assets/ImageFadeOut.cs
+++ Assets/ImageFadeOut.cs
@@ -1,17 +1,64 @@
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
+using FMODUnity;
 
-public class ImageFadeOut : MonoBehaviour
+public class ScreenFader : MonoBehaviour
 {
-    [SerializeField] private Image imageToFade;
+    [Header("Fade Settings")]
+    [SerializeField] private CanvasGroup blackOverlay;
     [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private float blackScreenDuration = 1f;
 
-    public void FadeOut()
+    [Header("Audio")]
+    [SerializeField] private EventReference sleepSound;
+
+    [Header("Player Control")]
+    [SerializeField] private CharacterMovement characterMovement; // Assign in Inspector
+
+    private float originalSpeed;
+
+    private void Start()
     {
-        if (imageToFade != null)
+        blackOverlay.alpha = 0f;
+    }
+
+    private void OnEnable()
+    {
+        FadeToBlackAndBack();
+    }
+
+    public void FadeToBlackAndBack()
+    {
+        RuntimeManager.PlayOneShot(sleepSound);
+
+        if (characterMovement != null)
         {
-            imageToFade.DOFade(0f, fadeDuration);
+            originalSpeed = characterMovement.Speed;
+            characterMovement.Speed = 0f;
         }
+
+        Sequence fadeSequence = DOTween.Sequence();
+
+        fadeSequence.Append(blackOverlay.DOFade(1f, fadeDuration))
+                    .AppendInterval(blackScreenDuration)
+                    .Append(blackOverlay.DOFade(0f, fadeDuration))
+                    .OnComplete(() =>
+                    {
+                        if (characterMovement != null)
+                            characterMovement.Speed = originalSpeed;
+
+                        gameObject.SetActive(false);
+                    });
+    }
+
+    public void FadeToBlack()
+    {
+        RuntimeManager.PlayOneShot(sleepSound);
+        blackOverlay.DOFade(1f, fadeDuration);
+    }
+
+    public void FadeFromBlack()
+    {
+        blackOverlay.DOFade(0f, fadeDuration);
     }
 }
--- Assets/Scripts/MonoBehaviours/Controllers/ChildController.cs
+++ Assets/Scripts/MonoBehaviours/Controllers/ChildController.cs
@@ -1,31 +1,34 @@
-using System.Collections.Generic;
-
 using UnityEngine;
-using UnityEngine.Events;
 
 public class ChildController : Singleton<ChildController>
 {
-    [SerializeField]
-    private GameObject humanForm;
+    [SerializeField] private GameObject humanForm;
+    [SerializeField] private Animator animator;
 
-    /// <summary>
-    /// Spawn position of the child.
-    /// </summary>
+    [Header("Unity 6 Cinemachine Camera (Optional)")]
+    [SerializeField] private MonoBehaviour cameraToDisableWhileMoving; // Use actual type if known
+
+    private CharacterMovement movement;
     private Vector3 spawnPosition;
 
-    protected override void Awake()
+    private void Awake()
     {
-        base.Awake();
-
+        movement = GetComponent<CharacterMovement>();
         spawnPosition = transform.position;
 
         GameManager.Instance.OnDayBegin.AddListener(GameManager_OnDayBegin);
         GameManager.Instance.OnNightBegin.AddListener(GameManager_OnNightBegin);
     }
 
-    private void OnEnable() => humanForm.SetActive(true);
+    private void OnEnable()
+    {
+        humanForm.SetActive(true);
+    }
 
-    private void OnDisable() => humanForm.SetActive(false);
+    private void OnDisable()
+    {
+        humanForm.SetActive(false);
+    }
 
     private void GameManager_OnDayBegin()
     {
@@ -36,5 +39,26 @@ public class ChildController : Singleton<ChildController>
     private void GameManager_OnNightBegin()
     {
         enabled = false;
+    }
+
+    private void Update()
+    {
+        bool isMoving = !movement.IsMoving();
+        animator.SetBool("isMoving", isMoving);
+
+        // Flip child based on X movement
+        float x = movement.Velocity.x;
+        if (Mathf.Abs(x) > CharacterMovement.ZeroSpeedThreshold)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = x > 0 ? Mathf.Abs(scale.x) : -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+
+        // Disable Unity 6 Cinemachine camera while moving
+        if (cameraToDisableWhileMoving != null)
+        {
+            cameraToDisableWhileMoving.enabled = !isMoving;
+        }
     }
 }
