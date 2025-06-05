ea06496be3b33f6f3678b91ae034647417ef6aec
Matěj Hes <hesmatej1@gmail.com>
2025-06-06 00:47:15 +0200

    ALL Drawings + things to appear third night

--- Assets/KeyPickup.cs
+++ Assets/KeyPickup.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class KeyPickup : MonoBehaviour
 {
     public string keyID; // e.g., "RedKey", "BlueKey"
-    [SerializeField] private GameObject objectToActivate; // Assign your canvas or any GameObject here
+
+    [SerializeField] private GameObject objectToActivate;    // Assign in Inspector
+    [SerializeField] private GameObject objectToDeactivate;  // Assign in Inspector
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,11 +14,12 @@ public class KeyPickup : MonoBehaviour
             PlayerKeys.Instance.AddKey(keyID);
 
             if (objectToActivate != null)
-            {
                 objectToActivate.SetActive(true);
-            }
 
-            Destroy(gameObject);
+            if (objectToDeactivate != null)
+                objectToDeactivate.SetActive(false);
+
+            Destroy(gameObject); // Remove the key pickup object
         }
     }
 }
